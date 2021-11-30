446276537243ae137270ee6b38233d8d87eac803
Y-T-G <32206511+Y-T-G@users.noreply.github.com>
2021-12-01 02:04:27 +0800

    Add title and description

--- app.py
+++ app.py
@@ -17,8 +17,11 @@ def predict(img):
     img = PILImage.create(img)
     pred,pred_idx,probs = learn_inf.predict(img)
     return {labels[i]: float(probs[i]) for i in range(len(labels))}
-
-gr_interface = gr.Interface(fn=predict, inputs=gr.inputs.Image(shape=(512, 512)),outputs=gr.outputs.Label(num_top_classes=len(labels)), interpretation="default")
+title="AI Based COVID-19 Mortality Risk Assessment"
+description="""This app allows you to find the mortality risk of a COVID-19 patient by just using a chest X-Ray. Just drop your image below and click "Submit"."""
+gr_interface = gr.Interface(fn=predict, inputs=gr.inputs.Image(shape=(224, 224)),outputs=gr.outputs.Label(num_top_classes=len(labels)), interpretation="default",
+                            title=title, description=description)
+gr_interface.launch(share=True)
 gr_interface.launch()
 
 
