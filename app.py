0822e00fa245423e8ff6f88b83cf638a0b6fad14
raashidsalih <61780547+raashidsalih@users.noreply.github.com>
2021-12-05 06:37:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def predict(img):
     pred,pred_idx,probs = learn_inf.predict(img)
     return {labels[i]: float(probs[i]) for i in range(len(labels))}
 title="AI Based COVID-19 Mortality Risk Assessment"
-description="""This app allows you to find the mortality risk of a COVID-19 patient by just using a chest X-Ray. Just drop your image below and click "Submit"."""
+description="""This tool is designed to help assess the mortality risk of a COVID-19 patient based on their chest X-ray. Mortality risk can be defined as the likelihood of an individual dying. A ResNet-18 model was trained and tested on datasets procured from Cohen et al. and Stony Brook University. Just upload an image below and click "Submit" to obtain the model's prediction."""
 gr_interface = gr.Interface(fn=predict, inputs=gr.inputs.Image(shape=(224, 224)),outputs=gr.outputs.Label(num_top_classes=len(labels)), interpretation="default",
                             title=title, description=description)
 gr_interface.launch(share=True)
