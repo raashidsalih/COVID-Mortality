5d39b025d60313db697afb9584fc2ea4d74e41a8
Y-T-G <yaseensinbox@gmail.com>
2022-01-01 02:58:10 +0800

    Add sample xrays

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import gradio as gr
+import glob
 from fastai.vision.all import load_learner, PILImage
 
 
@@ -17,6 +18,8 @@ learn_inf = load_learner('models/res18-tcia.pth')
 
 labels = ['Low Risk', 'High Risk']
 
+sample_xrays = glob.glob("examples/*")
+
 
 def predict(img):
     img = PILImage.create(img)
@@ -25,8 +28,21 @@ def predict(img):
 
 
 title = "AI Based COVID-19 Mortality Risk Assessment"
-description = """This tool is designed to help assess the mortality risk of a COVID-19 patient based on their chest X-ray. Mortality risk can be defined as the likelihood of an individual dying. A ResNet-18 model was trained and tested on datasets procured from Cohen et al. and Stony Brook University. Just upload an image below and click "Submit" to obtain the model's prediction."""
-gr_interface = gr.Interface(fn=predict, inputs=gr.inputs.Image(shape=(224, 224)), outputs=gr.outputs.Label(num_top_classes=len(labels)), interpretation="default",
-                            title=title, description=description)
+description = """This tool is designed to help assess the mortality risk
+of a COVID-19 patient based on their chest X-ray. Mortality risk can be
+defined as the likelihood of an individual dying. A ResNet-18 model was
+trained and tested on datasets procured from Cohen et al. and Stony Brook
+University. Just upload an image below and click "Submit" to obtain the
+model's prediction."""
+gr_interface = gr.Interface(fn=predict,
+                            inputs=gr.inputs.Image(
+                              shape=(224, 224)
+                              ),
+                            outputs=gr.outputs.Label(
+                              num_top_classes=len(labels)
+                            ),
+                            interpretation="default",
+                            title=title, description=description,
+                            examples=sample_xrays)
 gr_interface.launch(share=True)
 gr_interface.launch()
